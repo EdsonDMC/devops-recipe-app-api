5162ac380942d0daa294ccd4d360a228d4de8825
edson <edson.damc@gmail.com>
2024-05-25 21:12:51 -0400

    terraform setup

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region = "us-east-1"
 
   default_tags {
-    tag = {
+    tags = {
       Environment = terraform.workspace
       Project     = var.project
       Contact     = var.contact
