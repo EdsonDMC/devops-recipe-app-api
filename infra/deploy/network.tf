1133a99ba8af737a1180726ba5049e8ea9772b4c
edson <edson.damc@gmail.com>
2024-06-11 22:28:58 -0400

    fix Name tag

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -1,15 +1,20 @@
+##########################
+# Network infrastructure #
+##########################
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
 
-##########################################################
-# Policy for Terraform backend to S3 and DynamoDB access #
-##########################################################
+#########################################################
+# Internet Gateway needed for inbound access to the ALB #
+#########################################################
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+
   tags = {
-    name = "${local.prefix}-main"
+    Name = "${local.prefix}-main"
   }
 }
